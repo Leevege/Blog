0654e9c36ec7483b3be811e440f856328dcb8ba3
Leevege <LeevegeKennedy@gmail.com>
2019-08-22 22:03:46 +0800

    优化语法

--- js/main.js
+++ js/main.js
@@ -1,116 +1,113 @@
 'use strict';
-// 获取工具区域所有标签
-let Tag = document.getElementsByClassName('nav-bar')[0];
-let RealTags = Tag.getElementsByTagName('a');
+localStorage ? console.log('你的浏览器支持本地数据存储功能') : alert('你的浏览器不支持存储数据！');
 // 载入工具区域主体
-let iWantWords = document.getElementById('typeWords');
-// 在控制台输出信息,如果可以打印说明载入无误
-console.log(iWantWords);
-let outPut = document.querySelector('.output');
-const keyWordArrele = document.getElementById('keyWordAll'); // 选中关键词列表元素
-let allkeyWords = keyWordArrele.innerHTML;
+const outPut = document.querySelector('.output');
+const wholeText = document.getElementById('wholeText');
+const keyWordList = document.getElementById('keyWordAll');
+let allkeyWords = keyWordList.innerHTML;
+const keyWordin = document.getElementById('keyWords');
 // 分别定义提交和扫描关键词函数
-let oSub = document.getElementById('Sub1');
+const oSub = document.getElementById('Sub1');
 oSub.onclick = submit;
-let oScan = document.getElementById('Scan1');
+const oScan = document.getElementById('Scan1');
 oScan.onclick = scan;
-  let scanwords = [
-    // 日后从数据库中导入关键词
-    '激情',
-    '能承受',
-    '压力',
-    '狼性',
-    '活力',
-    '奋斗',
-    '美女',
-    '月入',
-    '学习',
-    '挑战',
-    '吃苦',
-    '能力',
-    '梦想',
-    '理想',
-    '未来',
-    '期权',
-    '主动',
-    '领导',
-    '弹性',
-    '加班',
-    '抗压',
-    '面议',
-    '激励',
-    '团队精神',
-    '应届毕业',
-    '积极',
-    '信心',
-    '！'
-  ];
-  const buttonInLi = '<input type="button" class="delete" value="x" onclick="removeElement(this)">'; // 提前定义删除按钮
-  // 首先将数据库中的关键词都打印出来
-  for (let i = 0; i < scanwords.length; i++) {
-    allkeyWords += '<li>' + scanwords[i] + buttonInLi + '</li>';
+let storeWords = [];
+storeWords = JSON.parse(localStorage.getItem(1)) || storeWords;
+const buttonInLi = '<input type="button" class="delete" value="x" onclick="removeElement(this)">';
+// 提前定义删除按钮
+keyWordList.innerHTML = refresh();
+/*
+'激情',
+'能承受',
+'压力',
+'狼性',
+'活力',
+'奋斗',
+'美女',
+'月入',
+'学习',
+'挑战',
+'吃苦',
+'能力',
+'梦想',
+'理想',
+'未来',
+'期权',
+'主动',
+'领导',
+'弹性',
+'加班',
+'抗压',
+'面议',
+'激励',
+'团队精神',
+'应届毕业',
+'积极',
+'信心',
+'！'
+*/
+function refresh() {
+  for (let i = 0; i < storeWords.length; i++) {
+    allkeyWords += `<li>${storeWords[i]}${buttonInLi}</li>`;
   }
-  // 输出结果
-  keyWordArrele.innerHTML = allkeyWords;
-  // 扫描关键词函数
-function scan(){
+  return allkeyWords;
+}
+// 扫描关键词函数
+function scan() {
   let result = [];
   // 循环检查关键词并记录
-  for (let i = 0; i < scanwords.length; i++) {
-  	if (iWantWords.value.match(scanwords[i]) === null){
-  		continue;
-  	} else {
-  		result.push(iWantWords.value.match(scanwords[i]));
-  	}
+  for (let i = 0; i < storeWords.length; i++) {
+    if (wholeText.value.match(storeWords[i]) === null) {
+      continue;
+    } else {
+      result.push(wholeText.value.match(storeWords[i]));
+    }
   }
   console.log(result);
   // 根据是否存在关键词输出结果
-  if(result.length === 0){
+  if (result.length === 0) {
     outPut.textContent = '这段内容中没查询到任何关键词！';
-  } else{
+  } else {
     outPut.textContent = '这段内容中查询到以下关键词：' + result;
   }
-  iWantWords.focus();
+  wholeText.focus();
 }
 // 提交新关键词函数
-function submit(){
+function submit() {
   // 以空格切分关键词
-  let temp = document.getElementById('keyWords').value.split(' ');
-  (temp === '') ? null : (function(){for (let i = 0; i < temp.length; i++){scanwords.push(temp[i]);}}());
-  scanwords = Array.from(new Set(scanwords));
+  let temp = keyWordin.value;
+  if (temp === '') return;
+  temp = temp.split(' ').filter((words) => words.trim());
+  storeWords.push(...temp);
+  storeWords = Array.from(new Set(storeWords));
   allkeyWords = '';
-  for (let i = 0; i < scanwords.length; i++) {
-    allkeyWords += '<li>' + scanwords[i] + buttonInLi + '</li>';
-  }
-  keyWordArrele.innerHTML = allkeyWords;
-  keyWordArrele.scrollTop = keyWordArrele.scrollHeight;
+  keyWordList.innerHTML = refresh();
+  keyWordList.scrollTop = keyWordList.scrollHeight;
+  localStorage.setItem(1, JSON.stringify(storeWords));
   // 使用户输入新关键词后能看到新关键词
-  document.getElementById('keyWords').value = '';
-  document.getElementById('keyWords').focus();
+  keyWordin.value = '';
+  keyWordin.focus();
   // 每次提交关键词之后自动聚焦
 }
 // 删除关键词函数
-function removeElement(_element){
+function removeElement(_element) {
   let _parentElement = _element.parentNode;
   // 根据节点内容删除数组内容
-  scanwords.splice(scanwords.indexOf(_parentElement.innerText),1);
+  storeWords.splice(storeWords.indexOf(_parentElement.innerText), 1);
   _parentElement.remove();
+  localStorage.setItem(1, JSON.stringify(storeWords));
 }
 //使用回车键输入关键词
-document.getElementById('keyWords').onkeydown = function (event) {
+keyWordin.onkeydown = function (event) {
   let e = event || window.event;
-    if (e && e.keyCode === 13){
+  if (e && e.keyCode === 13) {
     submit();
-    }
+  }
 }
 // ctrl和回车同时按下时扫描
-iWantWords.onkeydown = function (event) {
+wholeText.onkeydown = function (event) {
   let e = event || window.event;
-    if (e && e.keyCode === 13 && 17){
+  if (e && e.keyCode === 13 && 17) {
     scan();
-    }
+  }
 }
-
-// for(let i = 0; i<RealTags.length;i++){
-//   RealTags[i].onclick = console.log('yes!');
-// }
